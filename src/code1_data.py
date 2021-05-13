4c4e4a49c7fa910654955fc17cd57378dc87bf40
CristinaCallejo <cristinapazcallejo@gmail.com>
2021-05-13 13:47:31 +0200

    imported fer2013.csv, added column with emotion names, converted values in pixels column into np.arrays

---
+++ src/code1_data.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import sys
+sys.path.append('../src')
+
+def import_csv (string):
+    dataframe_orig = pd.read_csv(string,encoding = "ISO-8859-1")
+    return dataframe_orig
