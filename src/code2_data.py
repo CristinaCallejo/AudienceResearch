a671059a265ecf69118884ebf9ab8259860d9baf
“CristinaCallejo” <cristinapazcallejo@gmail.com>
2021-05-21 12:45:33 +0200

    model done

--- src/code2_data.py
+++ src/code2_data.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 from tensorflow.python.keras.saving.hdf5_format import load_model_from_hdf5
 sys.path.append('../src')
@@ -9,93 +10,39 @@ import pandas as pd
 from pathlib import Path
 from PIL import Image
 import cv2
+import streamlit as st
 
 import json
 from keras_preprocessing.image import ImageDataGenerator, load_img, img_to_array
 from keras.models import Sequential, model_from_json, load_model
 from keras import backend as K
-#import src.preds as mod
+
 import h5py
 
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+model = h5py.File('models/model_v4red.h5', 'r')
 
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+print(face_cascade)
 n = 0
 counter_fotos = 0
-"""
-def predict_(path):
-
-    input_img=cv2.imread(str(path))
-    input_img2=cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(input_img2, 1.25, 6)
-    
-    x,y,w,h = faces[0]
-    img_data1= input_img2[y:y+h,x:x+w]
-    img_data2= cv2.resize(img_data1,(48,48))
-    
-    img_data3 = np.stack(img_data2)
-    img_data4 = img_data3 / 255.0
-    
-    return img_data4
-
-    # ...formatting input 
-    binary_emo = cc2.transf_imported_Image(uploaded_file)
-    
-    # ...let's see what science can tell us!
-    being_happy = cc2.playingGod(model.predict(binary_emo)[0])
-
-    try: 	model_path = './models/model.h5'
-	        model_weights_path = './models/weights.h5'
-    
-    happiness = being_happy[0]
-    not_quite_so_much_happiness = being_happy[1]
-    
-    if being_happy[0] > 0.7:
-
-
-"""
-
-""
-
-def playingGod(happiness):
-    model = load_model()
-    
-    with open()
-    model.load_weights(weights_path)
-    Y_pred = model.predict(X_test)
-    # Convert predictions classes to one hot vectors 
-    Y_pred_classes = np.argmax(Y_pred,axis = 1) 
-    # Convert validation observations to one hot vectors
-    print(Y_pred_classes)
-    Y_true = np.argmax(y_test,axis = 1)
-    print(Y_true)
-    # compute the confusion matrix
-    cm = confusion_matrix(Y_true, Y_pred_classes)
-    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    # plot the confusion matrix
-    f,ax = plt.subplots(figsize=(8, 8))
-    sns.heatmap(cm, annot=True, linewidths=0.01,cmap="YlGnBu",linecolor="gray", fmt= '.1f',ax=ax)
-    plt.xlabel("Predicted Label")
-    plt.ylabel("True Label")
-    plt.title("Confusion Matrix")
-    plt.show()
-    
-
-
-def transf_imported_Image(uploaded_file):
+def chorro(uploaded_file):
     counter_faces = 0
+        
     img = Image.open(uploaded_file)
-    new = img.save(f"demo/{counter_faces}.jpg")
+    new = img.save('demo/f"{counter_faces}".jpg')
+    st.write("image saved")
+    new_img = Image.open('demo/f"{counter_faces}".jpg')
 
     #input_img1 = cv2.imread(f"demo/{counter_faces}.jpg")
-    input_img1 = cv2.imread(new)
+    input_img1 = cv2.imread('demo/f"{counter_faces}".jpg')
     input_img2 = cv2.cvtColor(input_img1, cv2.COLOR_BGR2GRAY)
     input_img3 = input_img2.copy()
-    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-    faces = face_cascade.detectMultiScale(input_img2, 1.25, 6)
-        
+    
+    faceClass = cv2.CascadeClassifier("src/haarcascade_frontalface_default.xml")
+    faces = faceClass.detectMultiScale(input_img2,scaleFactor=1.1, minNeighbors=7)       
     for (x,y,w,h) in faces:
-        
+            
+        st.write("got faces")
         counter_faces += 1
         img_data1 = input_img3 [y:y+h,x:x+w]
         img_data2 = cv2.resize (img_data1, (48, 48))
@@ -105,88 +52,29 @@ def transf_imported_Image(uploaded_file):
             img_data4, axis=0).reshape(
                 np.expand_dims(
                     img_data4, axis=0).shape[0], 48, 48, 1)
-        
-        cv2.imwrite(f"demo/{counter_fotos}_{counter_faces}.jpg", img_data2)
-        
+            
+        cv2.imwrite(f"demo/{n}_{counter_faces}.jpg", img_data2)
+            
         img_datashow = img_data3*255
         img_show = Image.fromarray(img_datashow)
-        img = Image.open("images_support/cover1.jpeg")
-        img_show.save(f"demo/{counter_fotos}_{counter_faces}_a.jpg")
+        img = Image.open("images_support/cover1.jpeg") 
+        img_show.save(f"demo/{n}_{counter_faces}_a.jpg")
+
+
+        with open('models/model_v4red_weights.h5.json','r') as f:
+            model_json = json.load(f)
+            model = model_from_json(model_json)
+            model.load_weights('models/model_v4red.h5')
+            #model = h5py.File('models/model_v3.hdf5', 'r')
+            EM = model.predict(img_data5)[0]
+            model_red = load_model('model_v4red.h5')
+
+            counter_faces = 0
             
-        arr_for_model = img_data5
-        counter_faces = 0
-        
-        print("terminado de proc")
-        
-        return arr_for_model
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        return img_data5
-        img_datashow = img_data3*255
-        img_show = Image.fromarray(img_datashow)
-        #file_to_save = file.name.replace(".",f"_face{counter_faces}.")
-        img_show.save(f"demo/{counter_fotos}_{counter_faces}_a.jpg")
-            
-        counter_faces = 0
-            
-        arr_for_model = img_data5
-        print("terminado de proc")
-        return arr_for_model
-"""
-
-            def load_trained_model(weights_path):
-        model = create_model()
-   model.load_weights(weights_path)
-   
-     
-
-
-
-
-
-
-def carga_data():
-    data = pd.read_csv("data/clean.csv")
-    return data
-
-
-def grafico_barras_st():
-    data = carga_data()
-    data = data.groupby("character_name").agg({"character_name":'count'}).rename(columns={"character_name":"character_name", "character_name":"número de frases"}).reset_index().set_index("character_name", drop=True)
-    return data
-
-
-def lista_personajes():
-    data = carga_data()
-    return list(data.character_name.unique())
-
-
-def grafico(personaje):
-    data = carga_data()
-    data = data[(data["character_name"]== f"{personaje}")]
-    return data
-
-def bar_2():
-    data = carga_data()
-    data = data.groupby("character_name").agg({'polarity': 'mean'}).reset_index().set_index("character_name", drop=True)
-    return data
-
-
-conv to arr
-
-"""
+            happy = EM[0]
+            unhappy = EM[1]
+            plt.imshow(Image.fromarray(EM.squeeze()*255))
+            st.write("The prediction is… happy:{0:.5f} other:{1:.5f}".format(EM[0],EM[1]))
+    return "TO BE CONTINUED"   
+                
+       
--- st_main.py
+++ st_main.py
@@ -16,9 +16,9 @@ import matplotlib.pyplot as plt
 import cv2
 import src.code2_data as cc2
 #import src.code3_data as cc3
-from tensorflow.keras.models import Sequential
+
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.models import load_model, model_from_json
+from tensorflow.keras.models import Sequential, load_model, model_from_json
 from tensorflow.keras.metrics import categorical_accuracy
 import tensorflow.keras.backend as K
 import streamlit.components.v1 as components
@@ -27,7 +27,7 @@ import h5py
 
 
 
-model = h5py.File('model_v3.hdf5', 'r')
+model = h5py.File('models/model_v3.hdf5', 'r')
 
 # LANDING
 img = Image.open("images_support/cover1.jpeg")
@@ -44,7 +44,7 @@ st.write(
 
 # INPUT IMAGE FROM USER
 uploaded_file = st.file_uploader(
-    "Drag & Drop your demo image here...", 
+    "each image goes in here...", 
     type = ['jpeg', 'jpg', 'png']
     )
 
@@ -53,165 +53,14 @@ n = 0
 if uploaded_file:
     n +=1
     foto = Image.open(uploaded_file)
-    foto.save(f"demo/foto{n}.jpg")
-    foto_pth = Path.cwd()/f"demo/foto{n}.jpg"
-    st.image(foto, caption="Let's find out", use_column_width=True)
+    fot = foto.save(f"demo/foto{n}.jpg")
+    fot_pth = Path.cwd()/f"demo/foto{n}.jpg"
+    st.image(foto, caption="Here it is", use_column_width=True)
     st.write("")
     st.write("Processing...")
 
 # IS THE USER/VIEWER HAPPY? 
    
     # ...formatting input 
-    binary_emo = cc2.transf_imported_Image(uploaded_file)
-    input_img = cv2.imread(uploaded_file)
-    
-    # ...let's see what science can tell us!
-    being_happy = cc2.playingGod(model.predict(binary_emo)[0])
-
-    try: 	model_path = './models/model.h5'
-	        model_weights_path = './models/weights.h5'
-    
-    happiness = being_happy[0]
-    not_quite_so_much_happiness = being_happy[1]
-    
-    if being_happy[0] > 0.7:
-        st.write(
-            '''
-            There is a f"{being_happy[0]}" chance that this user is having a HAPPY experience!
-            ''')
-    else:
-        st.write(
-            '''
-            The image processed leaves some doubts, but here are the results:
-            there is a f"{being_happy[0]}" chance that the user 
-            is having a good time vs f"{being_happy[1]}". 
-            It might be time to take a step back and consider exploring 
-            just how much tech has to offer in the industry:
-            your clients
-            your your product!
-            ''')
-
+    cc2.chorro(uploaded_file)
         
-""""
-    print(f"Probs -> happy:{binary_happiness[0]} unhappy:{binary_happiness[1]}")
-    if binay_hap[] > 0.8:
-        st.write("Llego mama")
-        image = blavblabla
-        st.image(image)
-    st.write('%s (%.2f%%)' % (label[1], label[2]*100))
-    
-    plt.subplot(121)
-    plt.imshow(input_img) # original pic
-
-    plt.subplot(122)
-    plt.imshow(Image.fromarray(PIC.squeeze()*255)) # transformed pic
-
-    
-"""
-"""
-if uploaded_file:
-    n+=1
-    PIC = ccst.transf_imported_Image(uploaded_file)
-    input_img=cv2.imread(uploaded_file) # get the array of the original pic
-
-    plt.subplot(121)
-    plt.imshow(input_img) # original pic
-
-    plt.subplot(122)
-    plt.imshow(Image.fromarray(PIC.squeeze()*255)) # transformed pic
-
-"""    
-"""
-PIC = transfImag2('foto.jpg') # transform pic
-input_img=cv2.imread('foto.jpg') # get the array of the original pic
-
-plt.subplot(121)
-plt.imshow(input_img) # original pic
-print(input_img.shape)
-plt.subplot(122)
-plt.imshow(Image.fromarray(PIC.squeeze()*255)) # transformed pic
-
-PIC = np.expand_dims(PIC,axis=0).reshape(np.expand_dims(PIC,axis=0).shape[0], 48, 48, 1)
-print(PIC.shape)
-pred2 = model_1.predict(PIC)[0]
-print("Probs -> happy:{0:.5f} unhappy:{1:.5f}".format(pred2[0],pred2[1]))
-
-happy = pred2[0]
-unhappy = pred2[1]
-
-
-
-st.write("""
-# Mi súper Dashboard
-#Con Jake el perro y Finn el humano lo pasaremos guaaaaay
-""")
-
-st.dataframe(dat.carga_data())
-
-st.write("""
-#Gragiquito de barras propio de streamlit
-""")
-
-
-st.dataframe(dat.grafico_barras_st())
-datos = dat.grafico_barras_st()
-st.bar_chart(datos)
-
-st.write("""
-#Gráfico de Plotly
-""")
-
-personaje = st.selectbox(
-
-    "Selecciona un personaje", dat.lista_personajes()
-)
-
-datagraf = dat.grafico(personaje)
-
-fig = px.line(datagraf, y="polarity", title = f"Evolución de la polaridad de {personaje}")
-
-st.plotly_chart(fig)
-
-st.write("""#Formulario de texto""")
-"""
-texto = st.text_input("Lo que tiene que introducir", "Texto por defecto")
-st.write("Ha introducido ", texto)
-
-
-st.write ("""#Gestor de archivos""")
-"""
-uploaded_file = st.file_uploader("Sube un csv")
-
-if uploaded_file: 
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
-
-foto = st.file_uploader("Sube una foto")
-if foto:
-    imagen = Image.open(foto)
-    imagen.save("data/foto.png")
-    st.write ("tu foto se ha subido correctamente")
-
-
-st.write("Columnas")
-
-#datines = dat.bar_2()
-
-col1,col2 = st.beta_columns([4,2])
-
-col1.subheader("El Gráfico")
-#col1.bar_chart(datines)
-
-col2.subheader("Los datos")
-#col2.write(datines)
-
-"""
-"""
-map_1 = folium.Map(location = [45.50935, -73.57225], zoom_start = 15)
-folium_static(map_1)
-
-
-archivo = codecs.open("data/mapa.html", "r")
-mapa = archivo.read()
-components.html(mapa, height = 550)
-"""
